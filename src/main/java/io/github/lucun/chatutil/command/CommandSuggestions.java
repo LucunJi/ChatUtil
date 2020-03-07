529f5a305dcc5b2bcaf985bddf98769980ec42cf
LucunJi <1063464500@qq.com>
2020-03-06 20:47:00 -0600

    v0.2

--- src/main/java/io/github/lucun/chatutil/command/CommandSuggestions.java
+++ src/main/java/io/github/lucun/chatutil/command/CommandSuggestions.java
@@ -1,17 +1,13 @@
 package io.github.lucun.chatutil.command;
 
-import com.google.common.collect.Maps;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.suggestion.SuggestionProvider;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import io.github.lucun.chatutil.setting.Settings;
-import net.minecraft.command.suggestion.SuggestionProviders;
 import net.minecraft.server.command.CommandSource;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.util.Identifier;
-import java.util.Map;
+
 import java.util.concurrent.CompletableFuture;
 
 public class CommandSuggestions {
--- src/main/java/io/github/lucun/chatutil/mixin/MixinClientPlayerEntity.java
+++ src/main/java/io/github/lucun/chatutil/mixin/MixinClientPlayerEntity.java
@@ -9,22 +9,14 @@ import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.network.Packet;
 import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
-import net.minecraft.text.Text;
 import net.minecraft.text.Texts;
 import net.minecraft.util.Formatting;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ClientPlayerEntity.class)
 public abstract class MixinClientPlayerEntity {
-    @Shadow @Final public ClientPlayNetworkHandler networkHandler;
-
-    @Shadow public abstract void sendChatMessage(String string);
-
-    @Shadow public abstract void addChatMessage(Text message, boolean bl);
 
     @Redirect(method = "sendChatMessage", at = @At(
             value = "INVOKE",
